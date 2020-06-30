976531edc8aa3dfe6b6f492f8aa3689f4db58111
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-29 23:16:02 -0400

    trying to authorize userId

--- ers/src/middleware/authoriz-middleware.ts
+++ ers/src/middleware/authoriz-middleware.ts
@@ -4,18 +4,26 @@ import { Request, Response, NextFunction } from "express";
 export function authorizationMiddleware(roles:string[] | number[]){
     return (req:Request, res:Response, next:NextFunction) => {
         let allowed = false
-       
+
+            
         for(const role of roles){
             if(req.session.user.roleDetails.role === role){
                 allowed = true
                 next()
-            }else if (req.session.user === req.params.userId){
-                allowed = true
-                next()
-
-            }
+            } 
             
         }
+
+        if (!allowed){
+            if (req.session.user === req.params.userId){
+                allowed = true
+                next()
+    
+            }
+        }
+
+        
+       
         if(!allowed){
             res.status(401).send("The Incoming Token has Expired - nocreds!")
         }
