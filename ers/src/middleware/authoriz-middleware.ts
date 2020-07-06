4b879a942c20fd67ad5effee379d94fbbe346308
Chrischale Panditharathne <cpanda27@bu.edu>
2020-07-06 12:23:55 -0400

    fixed reimbursement by user id

--- ers/src/middleware/authoriz-middleware.ts
+++ ers/src/middleware/authoriz-middleware.ts
@@ -4,23 +4,19 @@ import { Request, Response, NextFunction } from "express";
 export function authorizationMiddleware(roles:string[]){
     return (req:Request, res:Response, next:NextFunction) => {
         let allowed = false
-        console.log(req.session.user.userId)
-        console.log(req.params.id)
-        console.log(req.session.user.roleDetails.role)
+       
 
-        if (req.session.user.userId == req.params.id){
+        if (req.session.user.userId == (req.params.id || req.params.userId)){
             allowed = true
             next()
             
         } else {
             for(const role of roles){
-                console.log (role)
+               
                 if(req.session.user.roleDetails.role === role){
                     allowed = true
                     next()
                 } 
-                
-                
                 
             } 
 
